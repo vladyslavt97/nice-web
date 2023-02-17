5f5d5ea9244ea9f8191a9c9250c57177ce824756
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-02-17 14:29:42 +0100

    first commit with tailwind and header comp

---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+export default function Header({}: Props) {
+  return (
+    <div>Header</div>
+    // https://www.youtube.com/watch?v=urgi2iz9P6U&t=1294s
+    // 27:48
+  )
+}
