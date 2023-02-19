8985fc34a108ff582a1a80223505524e3a5b1e5f
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-02-19 20:44:24 +0100

    Trying with Link on email

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,5 @@
 import { motion } from 'framer-motion'
+import Link from 'next/link'
 import React from 'react'
 import { SocialIcon } from 'react-social-icons'
 type Props = {}
@@ -26,28 +27,30 @@ export default function Header({}: Props) {
         <SocialIcon url="https://www.linkedin.com/in/vladyslav-tsurkanenko/" fgColor='gray' bgColor='transparent'/>
       </motion.div>
 
-      <motion.div 
-        initial={{
-          x: 500,
-          opacity: 0,
-          scale: 0.5,
-        }}
-        animate={{
-          x: 0,
-          opacity: 1,
-          scale: 1,
-        }}
-        transition={{
-          duration: 1.5
-        }}
-        className='flex flex-row items-center text-gray-300 cursor-pointer'>
-        <SocialIcon className='cursor-pointer'
-        network='email'
-        fgColor='gray'
-        bgColor='transparent'
-        />
-        <p className='uppercase hidden md:inline-flex text-sm text-gray-400'>Get in touch</p>
-      </motion.div>
+        <motion.div 
+          initial={{
+            x: 500,
+            opacity: 0,
+            scale: 0.5,
+          }}
+          animate={{
+            x: 0,
+            opacity: 1,
+            scale: 1,
+          }}
+          transition={{
+            duration: 1.5
+          }}
+          className='flex flex-row items-center text-gray-300 cursor-pointer'>
+          <Link href="#contact">
+            <SocialIcon className='cursor-pointer'
+            network='email'
+            fgColor='gray'
+            bgColor='transparent'
+            />
+            <p className='uppercase hidden md:inline-flex text-sm text-gray-400'>Get in touch</p>
+          </Link>
+        </motion.div>
     </header>
   )
 }
