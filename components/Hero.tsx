74348c87fea032d0f1ff65b011cbffd90efc9cf1
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-02-19 09:59:25 +0100

    Starting about section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react'
 import { Cursor, useTypewriter } from 'react-simple-typewriter'
 import BackgroundCircles from './BackgroundCircles';
@@ -7,8 +9,8 @@ type Props = {}
 export default function Hero({}: Props) {
     const [text, count] = useTypewriter({
         words: ["Hi, I am Vlad",
-        "Guy-who-loves-Coffee.tsx",
-        "<ButLovesToCodeMore />",
+        "I like to code",
+        "Check out my portfolio",
     ],
     loop: true,
     delaySpeed: 2000,
@@ -17,12 +19,32 @@ export default function Hero({}: Props) {
   return (
     <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'>
         <BackgroundCircles />
-        {/* <img src="dora.jpg" alt="" /> */}
+        <Image 
+        className='relative rounded-full h-32 w-32 mx-auto object-covers'
+        src="/Vlad.png" alt="vlad" width={1000} height={1000}/>
 
-        <h1>
-            <span>{text}</span>
+      <div className='z-20'>
+        <h2 className='text-sm uppercase text-gray-500 tracking-[15px]'>Web Developer</h2>
+        <h1 className='text-5xl lg:6xl font-semibold scroll-px-10'>
+            <span className='mr-3'>{text}</span>
             <Cursor cursorColor='#F7AB0A'/>
         </h1>
+
+        <div className='pt-5'>
+          <Link href="#about">
+            <button className='heroButton'>About</button>
+          </Link>
+          <Link href="#experience">
+          <button className='heroButton'>Experience</button>
+          </Link>
+          <Link href="#skills">
+            <button className='heroButton'>Skills</button>
+          </Link>
+          <Link href="#projects">
+            <button className='heroButton'>Projects</button>
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }
